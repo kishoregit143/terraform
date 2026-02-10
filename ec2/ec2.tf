175e4aa878646fee721fe66fc3ac44caad187dce
kishore <pagadalamokisthu@gmail.com>
2026-02-10 12:14:44 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,7 +3,7 @@
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all_terf.id]
     tags = {
-       Name = "roboshop-1"
+       Name = "roboshop"
        Terraform = "true"
 
     }
