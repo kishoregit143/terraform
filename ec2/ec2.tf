991f1fc20c334619af82b9d4ab2d02af1574a12d
kishore <pagadalamokisthu@gmail.com>
2026-02-11 15:03:36 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,4 +1,4 @@
-#resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform" {
     ami = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all_terf.id]
